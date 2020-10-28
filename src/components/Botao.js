77cea6ed55db3e8606e8b6485e033b23a92429d7
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-28 19:20:44 -0200

    Criado classe CSS

--- src/components/Botao.js
+++ src/components/Botao.js
@@ -4,7 +4,8 @@ class Botao extends Component{
     render(){
         return(
             <div>
-
+                <button>Abrir Biscoito</button>
+                <h3 className='textoFrase'>Frase aleatoria..</h3>
             </div>
         )
     }
