c20596377e3309c4b722c761b20a1729af4813c0
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-28 19:17:28 -0200

    Criado array de frases

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Botao from './components/Botao'
 
 import biscoito from './assets/biscoito.png'
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props)
-    this.state={}
+    this.state = {}
+    this.frases =
+      [
+        'Siga os bons e aprenda com eles.',
+        'O bom-senso vale mais do que muito conhecimento.',
+        'O riso é a menor distância entre duas pessoas.',
+        'Deixe de lado as preocupações e seja feliz.',
+        'Realize o óbvio, pense no improvável e conquiste o impossível.',
+        'Acredite em milagres, mas não dependa deles.',
+        'A maior barreira para o sucesso é o medo do fracasso.'
+      ]
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <img src={biscoito} alt="biscoito"/>
-        <Botao/>
+        <img src={biscoito} alt="biscoito" />
+        <Botao />
       </div>
     )
   }
