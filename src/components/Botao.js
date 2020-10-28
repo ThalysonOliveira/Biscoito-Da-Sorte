5285471d8e48032189c675c9a59be4e0d481b036
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-10-28 19:52:00 -0200

    Passado nome do botao via props

--- src/components/Botao.js
+++ src/components/Botao.js
@@ -4,7 +4,7 @@ class Botao extends Component{
     render(){
         return(
             <div>
-                <button>Abrir Biscoito</button>
+                <button>{this.props.nome}</button>
                 <h3 className='textoFrase'>Frase aleatoria..</h3>
             </div>
         )
